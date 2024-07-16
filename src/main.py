2cbfabf087af0f7d7a78889f2ce6c1ea39ef01f6
devan191 <devan23191@iiitd.ac.in>
2024-07-17 00:27:57 +0530

    Add functionality of rendering board and chess pieces and also dragging and dropping chess pieces

---
+++ src/main.py
@@ -0,0 +1,65 @@
+import pygame
+import sys
+
+from const import *
+from game import Game
+
+class Main:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption("Chess")
+
+        self.game = Game()
+
+    def mainloop(self):
+        screen = self.screen
+        game = self.game
+        dragger = self.game.dragger
+        board = self.game.board
+
+        while True:
+            game.show_bg(screen)
+            game.show_pieces(screen)
+
+            if dragger.dragging:
+                dragger.update_blit(screen)
+
+            for event in pygame.event.get():
+                #click
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+                    #print(event.pos)
+                    #event.pos gives coordinates of wherever we are on screen
+                    clicked_row = dragger.mouseY // SQSIZE
+                    clicked_col = dragger.mouseX // SQSIZE
+                    #print(clicked_col,clicked_row)
+                    # if clicked square has a piece
+                    if board.squares[clicked_row][clicked_col].has_piece():
+                        piece = board.squares[clicked_row][clicked_col].piece
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
+
+                if event.type == pygame.MOUSEMOTION:
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        game.show_bg(screen)
+                        game.show_pieces(screen)
+                        dragger.update_blit(screen)
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if dragger.piece != None:
+                        dragger.undrag_piece()
+
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+
+
+            
+            pygame.display.update()
+
+main = Main()
+main.mainloop()
